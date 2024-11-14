7b25e68635ee073f6357ab7633198501d3b1ac22
ferid-kerimli <faridkaarimli@gmail.com>
2024-11-14 23:22:27 +0400

    Fixes

--- Base64ToImage/Base64ToImageApp.Designer.cs
+++ Base64ToImage/Base64ToImageApp.Designer.cs
@@ -8,7 +8,6 @@ partial class Base64ToImageApp
     private Button btnConvert;
     private Button btnClear;
     private RichTextBox txtBase64Input;
-    private PictureBox pictureBox;
     private Label lblInstructions;
 
     protected override void Dispose(bool disposing)
@@ -21,70 +20,70 @@ partial class Base64ToImageApp
         base.Dispose(disposing);
     }
 
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
     private void InitializeComponent()
     {
-        this.btnConvert = new Button();
-        this.btnClear = new Button();
-        this.txtBase64Input = new RichTextBox();
-        this.pictureBox = new PictureBox();
-        this.lblInstructions = new Label();
-        ((ISupportInitialize)(this.pictureBox)).BeginInit();
-        this.SuspendLayout();
-
-        // Convert Button
-        this.btnConvert.Location = new Point(20, 150);
-        this.btnConvert.Name = "btnConvert";
-        this.btnConvert.Size = new Size(100, 30);
-        this.btnConvert.TabIndex = 0;
-        this.btnConvert.Text = "Convert";
-        this.btnConvert.UseVisualStyleBackColor = true;
-        this.btnConvert.Click += new EventHandler(this.btnConvert_Click);
-
-        // Clear Button
-        this.btnClear.Location = new Point(130, 150);
-        this.btnClear.Name = "btnClear";
-        this.btnClear.Size = new Size(100, 30);
-        this.btnClear.TabIndex = 1;
-        this.btnClear.Text = "Clear";
-        this.btnClear.UseVisualStyleBackColor = true;
-        this.btnClear.Click += new EventHandler(this.btnClear_Click);
-
-        // RichTextBox for Base64 Input
-        this.txtBase64Input.Location = new Point(20, 40);
-        this.txtBase64Input.Multiline = true;
-        this.txtBase64Input.Name = "txtBase64Input";
-        this.txtBase64Input.Size = new Size(400, 100);
-        this.txtBase64Input.TabIndex = 3;
-
-        // PictureBox for displaying image
-        this.pictureBox.Location = new Point(20, 200);
-        this.pictureBox.Name = "pictureBox";
-        this.pictureBox.Size = new Size(320, 240);
-        this.pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
-        this.pictureBox.TabIndex = 4;
-        this.pictureBox.TabStop = false;
-
-        // Label Instructions
-        this.lblInstructions.AutoSize = true;
-        this.lblInstructions.Location = new Point(20, 20);
-        this.lblInstructions.Name = "lblInstructions";
-        this.lblInstructions.Size = new Size(256, 15);
-        this.lblInstructions.TabIndex = 5;
-        this.lblInstructions.Text = "Enter Base64 string of an image and click Convert.";
-
+        btnConvert = new System.Windows.Forms.Button();
+        btnClear = new System.Windows.Forms.Button();
+        txtBase64Input = new System.Windows.Forms.RichTextBox();
+        lblInstructions = new System.Windows.Forms.Label();
+        SuspendLayout();
+        // 
+        // btnConvert
+        // 
+        btnConvert.Location = new System.Drawing.Point(23, 200);
+        btnConvert.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+        btnConvert.Name = "btnConvert";
+        btnConvert.Size = new System.Drawing.Size(114, 40);
+        btnConvert.TabIndex = 0;
+        btnConvert.Text = "Convert";
+        btnConvert.UseVisualStyleBackColor = true;
+        btnConvert.Click += btnConvert_Click;
+        // 
+        // btnClear
+        // 
+        btnClear.Location = new System.Drawing.Point(149, 200);
+        btnClear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+        btnClear.Name = "btnClear";
+        btnClear.Size = new System.Drawing.Size(114, 40);
+        btnClear.TabIndex = 1;
+        btnClear.Text = "Clear";
+        btnClear.UseVisualStyleBackColor = true;
+        btnClear.Click += btnClear_Click;
+        // 
+        // txtBase64Input
+        // 
+        txtBase64Input.Location = new System.Drawing.Point(23, 53);
+        txtBase64Input.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+        txtBase64Input.Name = "txtBase64Input";
+        txtBase64Input.Size = new System.Drawing.Size(457, 132);
+        txtBase64Input.TabIndex = 3;
+        txtBase64Input.Text = "";
+        // 
+        // lblInstructions
+        // 
+        lblInstructions.AutoSize = true;
+        lblInstructions.Location = new System.Drawing.Point(23, 27);
+        lblInstructions.Name = "lblInstructions";
+        lblInstructions.Size = new System.Drawing.Size(339, 20);
+        lblInstructions.TabIndex = 5;
+        lblInstructions.Text = "Enter Base64 string of an image and click Convert.";
+        // 
         // Base64ToImageApp
-        this.AutoScaleDimensions = new SizeF(7F, 15F);
-        this.AutoScaleMode = AutoScaleMode.Font;
-        this.ClientSize = new Size(450, 460);
-        this.Controls.Add(this.lblInstructions);
-        this.Controls.Add(this.pictureBox);
-        this.Controls.Add(this.txtBase64Input);
-        this.Controls.Add(this.btnClear);
-        this.Controls.Add(this.btnConvert);
-        this.Name = "Base64ToImageApp";
-        this.Text = "Base64 to Image Converter";
-        ((ISupportInitialize)(this.pictureBox)).EndInit();
-        this.ResumeLayout(false);
-        this.PerformLayout();
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(514, 613);
+        Controls.Add(lblInstructions);
+        Controls.Add(txtBase64Input);
+        Controls.Add(btnClear);
+        Controls.Add(btnConvert);
+        Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+        Text = "Base64 to Image Converter";
+        ResumeLayout(false);
+        PerformLayout();
     }
 }
